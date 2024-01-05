bcd62e885487d190b6f1b82f07c3a2bb1119002e
Brett <opsfox245@gmail.com>
2024-01-05 14:02:28 -0600

    First

---
+++ MyConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using SylviaLib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Sylvia!");
+
+        Plant plant = new("Onion Plant");
+
+        plant.Debug();
+    }
+}
---
+++ SylviaLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace SylviaLib;
+
+public partial class Plant
+{
+    public Plant(string name){
+        Name = name;
+    }
+
+    public string Name {get; set;}
+
+    public string Save(){
+        return Name;
+    }
+}
+
+public partial class Plant{
+    public void Debug(){
+        Console.WriteLine(Name);
+    }
+}
